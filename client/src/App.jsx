93f372597483c6886777699bf3857435db69d6be
ALEXEY <avoronsib@gmail.com>
2023-10-30 14:08:10 +0700

    setup a client side

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import axios from "axios";
+import {saveAs} from "file-saver";
+import {useState} from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [name, setName] = useState("");
+  const [receiptId, setReceiptId] = useState(0);
+  const [price1, setPrice1] = useState(0);
+  const [price2, setPrice2] = useState(0);
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <input type="text" placeholder="Name" name="name" />
+      <input type="number" placeholder="Receipt Id" name="receiptId" />
+      <input type="text" placeholder="Price 1" name="name" />
+      <input type="text" placeholder="Price 2" name="name" />
+      <button className="btn">Dowload PDF</button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
